864b0d1218780fcdd26afa8f3c1381a294d31128
timou0911 <x0928048316@gmail.com>
2024-05-19 01:06:26 +0800

    Rearrange functions and imporve variables structure

--- src/Game.sol
+++ src/Game.sol
@@ -1,44 +1,111 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-interface IVRFProvider {
+interface IUpperControl {
     function requestRandomWord() external returns (uint256);
+    function endGame() external;
+    function setGameState(uint8 _gameState) external;
+    function getGameState() external returns (uint8);
 }
 
 /**
- * @title XXXXX
- * @author XXXXX
+ * @title Game logic
+ * @author Tim Ou
  * @notice XXXXX
  * @dev XXXXX
  */
 
+// TODO: Develop a way to save upper control's address before consturctor called
+// TODO: Change array players from dynamic to static
 contract Game {
-
     /** Errors */
+    error WrongFeeAmount();
+    error ParticipantFull();
+    error ParicipantDuplicated(address player);
+    error GameNotInWaitingState();
+    error GameNotInProgressState();
+    error CallerNotUpperControl(address caller, address upperControl);
 
     /** Type Declarations */
 
     /** State Variables */
-    address[5] private players;
-
+    uint8 private constant PARTICIPANT_NUMBER = 5;
+    uint256 private constant PARTICIPANT_FEE = 0.01 ether;
+    IUpperControl private immutable i_upperControl;
+    address[] private s_players;
 
     /** Events */
-
+    event GameJoined(address player);
+    event GameStart();
 
     /** Modifiers */
-
-
-    constructor() {
+    modifier onlyUpperControl() {
+        if (msg.sender != address(i_upperControl)) {
+            revert CallerNotUpperControl(msg.sender, address(i_upperControl));
+        }
+        _;
     }
 
-    function requestRandomWord() public returns (bool received) {
-
-        
+    modifier onlyGameStateInProgress() {
+        if (getState() != 2) {
+            revert GameNotInProgressState();
+        }
+        _;
     }
 
-    function decideRandomEvent(uint256 randomWord) public returns (bool received) {
+    modifier noDuplicatedPlayer() {
+        for (uint8 i = 0; i < PARTICIPANT_NUMBER; ++i) {
+            if (s_players[i] == msg.sender) {
+                revert ParicipantDuplicated(msg.sender);
+            }
+        }
+        _;
+    }
+
+    constructor(address gameCreator) payable onlyUpperControl() {
+        i_upperControl = IUpperControl(msg.sender);
+
+        s_players.push(gameCreator);
+    }
+
+    /**
+     * @notice Join an existing round
+     * @dev When particpant number is full, than create a new game instance
+     */
+    function enterGame() public payable noDuplicatedPlayer() {
+        if (msg.value != PARTICIPANT_FEE) {
+            revert WrongFeeAmount();
+        }
+        if (s_players.length >= PARTICIPANT_NUMBER) {
+            revert ParticipantFull();
+        }
+        if (getState() != 0) {
+            revert GameNotInWaitingState();
+        }
+
+        s_players.push(msg.sender);
+
+        emit GameJoined(msg.sender);
+
+        if (s_players.length == PARTICIPANT_NUMBER) {
+            i_upperControl.setGameState(2);
+            emit GameStart();
+        }
+    }
+
+    function decideRandomEvent(
+        uint256 randomWord
+    ) public onlyUpperControl() returns (bool received) {
+        // take mod of randomWord to decide event
         return received;
     }
 
+    function requestRandomWord() onlyGameStateInProgress() private {
+        i_upperControl.requestRandomWord();
+    }
+
     /** Getter Functions */
+    function getState() public returns (uint8) {
+        return uint8(i_upperControl.getGameState());
+    }
 }
---
+++ src/UpperControl.sol
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "./Game.sol";
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+/**
+ * @title Upper Control Contract to handle each game instances
+ * @author Tim Ou
+ * @notice Users can create a game from this contract
+ * @dev Implements Chainlink VRF. Handle game state and provide helper functions to game insatances
+ */
+
+contract UpperControl is VRFConsumerBaseV2 {
+    /** Errors */
+    error CallerNotGameContract();
+    error GameNotInProgress(GameState state);
+
+    /** Type Declarations */
+    enum GameState {NotGameContract, WaitingForPlayers, InProgress, Finished}
+
+    /** State Variables */
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint16 private constant PARTICIPANT_NUMBER = 5;
+    uint256 private constant PARTICIPANT_FEE = 0.01 ether;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private s_numWords = 1;
+
+    mapping(uint256 => address) private s_requestIdToGameAddress;
+    mapping(address => GameState) public s_gamesState;
+
+    /** Events */
+    event GameCreated(address gameAddress);
+    event RequestSent(uint256 requestId, uint32 s_numWords);
+    event RequestFulfilled(uint256 requestId, uint256[] randomWords);
+    event StateChange(GameState prev, GameState curr);
+    event GameFinished(address gameAddr);
+
+    /** Modifiers */
+    modifier onlyGameContract() {
+        if (s_gamesState[msg.sender] == GameState.NotGameContract) {
+            revert CallerNotGameContract();
+        }
+        _;
+    }
+
+    constructor(address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinator) {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+    }
+
+    /**
+     * @notice Create a round of game and become the first participant
+     * @dev Separate round creation and instance creation. Set game state to waiting participants
+     */
+    function createGame() public payable {
+        require(msg.value == PARTICIPANT_FEE, "Incorrect fee amount");
+
+        Game game = new Game{value: PARTICIPANT_FEE}(msg.sender);
+        require(address(game) != address(0), "Game instance creation failed.");
+
+        s_gamesState[address(game)] = GameState.WaitingForPlayers;
+
+        emit GameCreated(address(game));
+    }
+
+    /**
+     * @notice Chnage game state to finished
+     * @dev This function is called by game instances when game is over
+     */
+    function endGame() public onlyGameContract() {
+        if (s_gamesState[msg.sender] != GameState.InProgress) {
+            revert GameNotInProgress(s_gamesState[msg.sender]);
+        }
+
+        s_gamesState[msg.sender] = GameState.Finished;
+        
+        emit GameFinished(msg.sender);
+    }
+
+    /**
+     * @notice Called by game instances to request random words
+     * @dev Should first check caller is game instance or not
+     */
+    function requestRandomWords() external onlyGameContract() returns (uint256 requestId) {
+        requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            s_numWords
+        );
+
+        s_requestIdToGameAddress[requestId] = msg.sender;
+
+        emit RequestSent(requestId, s_numWords);
+    }
+    
+    /**
+     * @notice Called by Cahinlink node, than sends random words to game instances
+     * @dev Game contract should implement a function to receive random words
+     */
+    function fulfillRandomWords(uint256 _requestId, uint256[] memory _randomWords) internal override {
+        address gameAddress = s_requestIdToGameAddress[_requestId];
+        require(gameAddress != address(0), "Invalid request ID");
+        
+        bool received = Game(gameAddress).decideRandomEvent(_randomWords[0]);
+        require(received, "random words sent failed.");
+
+        emit RequestFulfilled(_requestId, _randomWords);
+    }
+
+    /** Setter Functions */
+    function setGameState(uint8 gameState) public onlyGameContract() {
+        GameState prevState = s_gamesState[msg.sender];
+        s_gamesState[msg.sender] = GameState(gameState);
+        emit StateChange(prevState, GameState(gameState));
+    }
+
+    /** Getter Functions */
+    function getGameState() public view returns (GameState) {
+        return s_gamesState[msg.sender];
+    }
+}
